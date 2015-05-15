e66696842a6daa4aaac875c5ef9c5740f04f7f86
Alexander Kashev <kav2k@ya.ru>
2015-05-15 02:05:00 +0200

    Add header, changelog viewer to options page
    Refactor the options page
    Move changelog to extension root

--- src/js/options.js
+++ src/js/options.js
@@ -1,12 +1,23 @@
-$("input[type=checkbox]").each(function() {
-  var el = this;
-  chrome.storage.local.get(el.id, function(data) {
-    console.log("Load: ", data);
-    el.checked = data[el.id];
+$(document).ready(function() {
+  $("input[type=checkbox]").each(function() {
+    var el = this;
+    chrome.storage.local.get(el.id, function(data) {
+      console.log("Load: ", data);
+      el.checked = data[el.id];
+    });
+  }).change(function() {
+    var data = {};
+        data[this.id] = this.checked;
+    console.log("Save: ", data);
+    chrome.storage.local.set(data);
   });
-}).change(function() {
-  var data = {};
-      data[this.id] = this.checked;
-  console.log("Save: ", data);
-  chrome.storage.local.set(data);
+
+  $.ajax("changelog.txt").done(function(text) {
+    $("#changelogText").text(text);
+  });
+
+  $("#changelogLink, #changelogHideLink").click(function() {
+    $("#optionsContainer, #changelogContainer").toggle();
+    $("#changelogLink, #changelogHideLink").toggle();
+  });
 });
